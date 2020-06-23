2a8e4f2e2b1fe81cc6c71fbc3595698747cf06fd
joaopfa <joaopfa@hotmail.com>
2020-06-23 20:07:01 -0300

    endpoint de post funcionando para postagem. Dá pra refatorar algo

--- src/data/PostDatabase.ts
+++ src/data/PostDatabase.ts
@@ -1,5 +1,21 @@
 import BaseDatabase from "./BaseDatabase";
+import Post from "../models/Post";
+
 
 export default class PostDatabase extends BaseDatabase {
   private static readonly TABLE_NAME: string = "labook_posts";
+
+  public async createPost (post: Post) {
+    
+    await this.getConnection()
+    .insert({
+      id: post.id,
+      photo: post.photo,
+      description: post.description,
+      created_at: post.created_at,
+      type: post.type,
+      user_id: post.user_id
+    })
+    .into(PostDatabase.TABLE_NAME)
+  }
 }
