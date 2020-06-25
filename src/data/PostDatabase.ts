4cdff9be57139bf0998072d24835ed7ce1ffc716
igordelesposti <igordelesposti@gmail.com>
2020-06-25 17:19:47 -0300

    endpoint feito curtir/descurtir

--- src/data/PostDatabase.ts
+++ src/data/PostDatabase.ts
@@ -1,27 +1,25 @@
 import BaseDatabase from "./BaseDatabase";
 import Post from "../models/Post";
 
-
 export default class PostDatabase extends BaseDatabase {
   private static readonly TABLE_NAME: string = "labook_posts";
+  private static readonly TABLE_NAME_LIKEDISLIKE: string = "labook_postlikes";
 
-  public async createPost (post: Post) {
-    
+  public async createPost(post: Post) {
     await this.getConnection()
-    .insert({
-      id: post.id,
-      photo: post.photo,
-      description: post.description,
-      created_at: post.created_at,
-      type: post.type,
-      user_id: post.user_id
-    })
-    .into(PostDatabase.TABLE_NAME);
-  };
+      .insert({
+        id: post.id,
+        photo: post.photo,
+        description: post.description,
+        created_at: post.created_at,
+        type: post.type,
+        user_id: post.user_id,
+      })
+      .into(PostDatabase.TABLE_NAME);
+  }
 
   public async getFeed(id: string): Promise<any> {
-
-    const feed =  await this.getConnection().raw (
+    const feed = await this.getConnection().raw(
       `
       SELECT labook_users.name, labook_posts.created_at, labook_posts.description, labook_posts.photo, labook_posts.type
       FROM labook_posts
@@ -37,17 +35,45 @@ export default class PostDatabase extends BaseDatabase {
       WHERE friend_id = "${id}")
       ORDER BY created_at DESC;
       `
-    )
-        return feed[0];
-  };
+    );
+    return feed[0];
+  }
 
   public async getFeedByType(type: string): Promise<any> {
-    const result = await this.getConnection().raw (
+    const result = await this.getConnection().raw(
       `
       SELECT * FROM labook_posts WHERE type = "${type}" ORDER BY created_at DESC;
       `
-    )
-    
+    );
+
     return result[0];
-  };
-};
+  }
+
+  public async getPostLiked(post_id: string, user_id: string): Promise<any> {
+    const result = await this.getConnection()
+      .select("*")
+      .from(PostDatabase.TABLE_NAME_LIKEDISLIKE)
+      .where({ post_id: post_id, user_id: user_id });
+
+    return result;
+  }
+
+  public async addLike(post_id: string, user_id: string): Promise<void> {
+    await this.getConnection()
+      .insert({
+        post_id,
+        user_id,
+      })
+      .into(PostDatabase.TABLE_NAME_LIKEDISLIKE);
+  }
+
+  public async removeLike(post_id: string, user_id: string): Promise<any>{
+    await this.getConnection()
+    .delete()
+    .from(PostDatabase.TABLE_NAME_LIKEDISLIKE)
+    .where({
+      post_id,
+      user_id
+    });
+  }
+}
